87e70243985b9041f8c5ab2f461331f3eba8926f
dennise.ferreira <ferreira.denisse@gmail.com>
2019-08-19 00:48:28 -0400

    Rutas, navbar, dependencias y deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: "", component: HomeComponent },
+  { path: "sobremi", component: AboutComponent },
+  { path: "contacto", component: ContactComponent },
+  { path: "proyectos", component: ProjectsComponent },
+  { path: "habilidades", component: SkillsComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
